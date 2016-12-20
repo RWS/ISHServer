a90663285ca0e4311a797314802ab158b549ebb9
Alex Sarafian <dev1978@outlook.com>
2016-12-20 12:56:09 +0100

    Import from ish bootstrap (#2)
    * Imported from ISHBootstrap.
    Automation improvements included.
    
    * Added module administration only for the cmdlets that need it
    
    * Improved the automation for publish
    
    * bumped version to 1.0
    
    * Bug fix publish automation
    
    * Improved readme

---
+++ Source/Modules/ISHServer/ISHServer.13.psm1
@@ -0,0 +1,71 @@
+<#
+# Copyright (c) 2014 All Rights Reserved by the SDL Group.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#>
+
+$importNames=@(
+    #region Helpers
+    "Get-ISHOSInfo"
+    "Get-ISHNETInfo"
+    "Test-ISHServerCompliance"
+    "Get-ISHServerFolderPath"
+    "Grant-ISHUserLogOnAsService"
+    "Get-ISHCOMPlus"
+    #endregion
+
+    #region Ports
+    "Set-ISHFirewallHTTPS"
+    "Set-ISHFirewallNETBIOS"
+    "Set-ISHFirewallOracle"
+    "Set-ISHFirewallHTTPS"
+    "Set-ISHFirewallSMTP"
+    "Set-ISHFirewallSQLServer"
+    #endregion
+
+
+    #region Global
+    "Get-ISHServerFolderPath"
+    "Get-ISHPrerequisites.ISH13"
+    "Initialize-ISHLocale"
+    "Initialize-ISHIIS"
+    "Initialize-ISHUser"
+    #endregion
+
+    #region Install
+    "Install-ISHToolDotNET.ISH13"
+    "Install-ISHToolVisualCPP.ISH13"
+    "Install-ISHToolJAVA"
+    "Install-ISHToolJavaHelp"
+    "Install-ISHToolHtmlHelp"
+    "Install-ISHToolAntennaHouse"
+    "Install-ISHToolOracleODAC"
+    "Install-ISHWindowsFeature"
+    "Install-ISHWindowsFeatureIISWinAuth"
+    "Install-ISHVisualBasicRuntime"
+    #endregion
+
+    #region Regional settings
+    "Initialize-ISHRegional"
+    "Initialize-ISHRegionalDefault"
+    #endregion
+
+    #region License
+    "Set-ISHToolAntennaHouseLicense"
+    #endregion
+)
+
+$importNames | ForEach-Object {. $PSScriptRoot\$_.ps1 }
+
+$exportedMemberNames=$importNames -replace "\.ISH[0-9]+",""
+Export-ModuleMember $exportedMemberNames
---
+++ Source/Modules/ISHServer/Test-RunningAsElevated.ps1
@@ -0,0 +1,47 @@
+<#
+# Copyright (c) 2014 All Rights Reserved by the SDL Group.
+# 
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#     http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#>
+
+function Test-RunningAsElevated
+{
+    [CmdletBinding()]
+    [OutputType([bool])]
+    param
+    (        
+        [parameter(Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCmdlet]
+        $StopCallerPSCmdlet=$null
+    )
+
+    $wid=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+    $prp=new-object System.Security.Principal.WindowsPrincipal($wid)
+    $adm=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+    $isAdmin=$prp.IsInRole($adm)
+    
+    if($StopCallerPSCmdlet)
+    {
+        if(-not $isAdmin)
+        {
+            $exception = New-Object System.InvalidOperationException "The current Windows PowerShell session is not running as Administrator. Start Windows PowerShell by  using the Run as Administrator option, and then try running the script again."
+            $errorRecord = New-Object System.Management.Automation.ErrorRecord $exception, "InvalidOperationException", ([System.Management.Automation.ErrorCategory]::PermissionDenied),$null
+            $StopCallerPSCmdlet.ThrowTerminatingError($errorRecord)
+        }
+    }
+    else
+    {
+        $isAdmin
+    }
+}
