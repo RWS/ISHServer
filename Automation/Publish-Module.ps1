952ec9dc40dad6b3647c03ffa7024f064c9932c5
Oleksandr Kuzmin <okuzmin@sdl.com>
2024-05-27 16:36:37 +0300

    Prepare ISHServer.15.1 release

--- Automation/Publish-Module.ps1
+++ Automation/Publish-Module.ps1
@@ -1,5 +1,5 @@
 <#
-# Copyright (c) 2023 All Rights Reserved by the RWS Group.
+# Copyright (c) 2024 All Rights Reserved by the RWS Group.
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@ switch ($PSCmdlet.ParameterSetName)
     'Public' {
         $publishDebug=$false
         $repository="PSGallery"
-        $moduleNamesToPublish+="ISHServer.15"
+        $moduleNamesToPublish+="ISHServer.15.1"
         break;
     }
     'Public+Internal' {
--- Source/Modules/ISHServer/Test-ISHServerCompliance.ps1
+++ Source/Modules/ISHServer/Test-ISHServerCompliance.ps1
@@ -1,5 +1,5 @@
 <#
-# Copyright (c) 2023 All Rights Reserved by the RWS Group.
+# Copyright (c) 2024 All Rights Reserved by the RWS Group.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
